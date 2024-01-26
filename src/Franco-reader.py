0eae5c89d865847ef06fec3424337e1bd73720cb
Geek du 25 <145868282+Geekdu25@users.noreply.github.com>
2024-01-26 15:16:25 +0100

    dsds

--- src/Franco-reader.py
+++ src/Franco-reader.py
@@ -13,20 +13,22 @@ else:
 #On charge le fichier test.frl
 fichier = open(filepath,'rt')
 ligne = 0
+variables = {}
 #Et on le lit
 for value in fichier.readlines():
   ligne = ligne + 1
-  #Et si une ligne du fichier commence par afficher...
-  if value.startswith('afficher "'):
-    fin = 10
-    try:
-      while value[fin] != '"':
-        fin = fin + 1
-        truc = value[10:fin]
-        #On affiche quelque chose
-      print(truc)
-    except IndexError:
-       print("Une erreur s'est produite.")
-       print(f"Avez-vous fermé l'instruction ligne {ligne} par des guillemets ?")
+  for mot in value.split():
+    #Et si une ligne du fichier commence par afficher...
+    if value.startswith('afficher("'):
+      fin = 10
+      try:
+        while value[fin] != '"':
+          fin = fin + 1
+          truc = value[10:fin]
+          #On affiche quelque chose
+        print(truc)     
+      except IndexError:
+         print("Une erreur s'est produite.")
+         print(f"Avez-vous fermé l'instruction ligne {ligne} par des guillemets ?")  
 #On finit le programme en fermant le fichier
 fichier.close()
