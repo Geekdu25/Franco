d86178a6ab67b434e4478dce89d803a378bcedcc
Geek du 25 <145868282+Geekdu25@users.noreply.github.com>
2024-01-30 09:30:19 +0100

    Add files via upload

--- src/Franco-reader.py
+++ src/Franco-reader.py
@@ -25,12 +25,13 @@ def read_frl():
       ligne = ligne + 1
       mots = []
       for mot in value.split():
-        mots.append(mot)
+        if mot != "":
+          mots.append(mot)
       if mots[0].startswith("#"):
-            pass  
+            pass
       if len(mots) > 1:
         if mots[0] == 'afficher':
-          print()  
+          print()
           affichage = True
           if mots[1].startswith('"'):
               try:
@@ -51,38 +52,61 @@ def read_frl():
                   print(f"Erreur, ligne {ligne}")
                   print(f"{mots[1]}, n'est pas défini.")
                   return False
-        elif not mots[0] in variables:
-          if mots[0][0].isdigit():
-            print(f"Erreur à la ligne {ligne}.")
-            print("Le nom d'une variable ne doit pas commencer par un chiffre.")
-            return False
-          else:
-            variables[mots[0]] = None
-            current_variable = mots[0]
-            recordVariable = True
-            if mots[1] != "=":
-              print(f"Erreur ligne {ligne}.")
-              print("Attention ! Vous essayez de définir une variable sans utiliser le signe =")
+        elif not mots[0] in variables and len(mots)>2:
+            if mots[0][0].isdigit():
+              print(f"Erreur à la ligne {ligne}.")
+              print("Le nom d'une variable ne doit pas commencer par un chiffre.")
               return False
             else:
-              recordVariable = False
-              if mots[2].isdigit():
-                  variables[current_variable] = int(mots[2])
-              elif mots[2].startswith('"') and mots[len(mots)-1].endswith('"'):
-                  tmp = mots[2:len(mots)]
-                  str_vide = ""
-                  for truc in tmp:
-                      if truc != tmp[len(tmp)-1]:
-                        str_vide = str_vide + truc + " "
+              variables[mots[0]] = None
+              current_variable = mots[0]
+              recordVariable = True
+              if mots[1] != "=":
+                print(f"Erreur ligne {ligne}.")
+                print("Attention ! Vous essayez de définir une variable sans utiliser le signe =")
+                return False
+              else:
+                  recordVariable = False
+                  if mots[2].isdigit() and len(mots)==3:
+                      variables[current_variable] = int(mots[2])
+                  elif mots[2].isdigit() and len(mots)==5:
+                    if mots[4].isdigit():
+                      if mots[3] == "+":
+                        variables[current_variable] = int(mots[2]) + int(mots[4])
+                      elif mots[3] == "-":
+                        variables[current_variable] = int(mots[2]) - int(mots[4])
+                      elif mots[3] == "*":
+                        variables[current_variable] = int(mots[2]) * int(mots[4])
+                      elif mots[3] == "/":
+                        variables[current_variable] = int(mots[2]) / int(mots[4])
                       else:
-                          str_vide = str_vide + truc
-                  variables[current_variable] = str(str_vide[1:len(str_vide)-1])
-              elif mots[2].startswith('"') and not mots[len(mots)-1].endswith('"'):
-                  print(f"Erreur ligne {ligne}")
-                  print("Veuillez fermer votre chaîne de caractères par un guillemet.")
-                  return False
-              if mots[2] in variables:
-                  variables[current_variable] = variables[mots[2]]
+                        print(f"Erreur ligne {ligne}")
+                        print("Opération mathématique non reconnue")
+                        return False
+                    else:
+                      print(f"Erreur ligne {ligne}")
+                      print("Pour faire une opération avec un nombre, il faut un deuxième nombre.")
+                      return False
+                  elif mots[2].startswith('"') and mots[len(mots)-1].endswith('"'):
+                      tmp = mots[2:len(mots)]
+                      str_vide = ""
+                      for truc in tmp:
+                          if truc != tmp[len(tmp)-1]:
+                            str_vide = str_vide + truc + " "
+                          else:
+                            str_vide = str_vide + truc
+                      variables[current_variable] = str(str_vide[1:len(str_vide)-1])
+                  elif mots[2].startswith('"') and not mots[len(mots)-1].endswith('"'):
+                      print(f"Erreur ligne {ligne}")
+                      print("Veuillez fermer votre chaîne de caractères par un guillemet.")
+                      return False
+                  if mots[2] in variables:
+                      variables[current_variable] = variables[mots[2]]
+
+        else:
+          print(f"Erreur ligne {ligne}")
+          print("Une erreur de syntaxe s'est produite.")
+          return False
     #On finit le programme en fermant le fichier
     fichier.close()
     return True
