fd3377e44be60dc700ea5d627db24c88a1a25612
Geek du 25 <145868282+Geekdu25@users.noreply.github.com>
2024-02-15 09:03:10 +0100

    Add files via upload

--- src/Franco-reader.py
+++ src/Franco-reader.py
@@ -17,142 +17,96 @@ fonctions = {}
 recordVariable = False
 
 def read_frl(fichier):
-    """
-    Fonction qui permet de lire un fichier .frl
-    -------------------------------------------
-    return -> bool
-    """
-    global ligne, current_variable, variables, commence, checksum, sauvegarde, fonctions, recordVariable
-    mots = []
-    #Et on lit le fichier...
-    #En le décomposant d'abord ligne par ligne...
-    for value in fichier.readlines():
-      ligne = ligne + 1
-      mots = []
-      #Puis mot par mot...
-      for mot in value.split():
-        if mot != "":
-          mots.append(mot)
-      if len(mots) == 0:
-        continue
-      else:
-        ok = execute_ligne(mots)
-        if ok == False:
-            return ok
-    if ok == None:
-      ok = True
-    return ok
+  """
+  Fonction qui décompose un fichier .frl ligne par ligne
+  ----------------------------------------------------------
+  fichier -> fichier
+  return -> bool
+  """
+  global ligne, current_variable, variables, commence, checksum, sauvegarde, fonctions, recordVariable
+  for truc in fichier.read().splitlines():
+    ligne = ligne + 1
+    execute_ligne(truc)
+  return True
 
 def execute_ligne(laligne):
     """
-    Fonction qui exécute les instruciotns d'une ligne
-    -------------------------------------------------
-    ligne -> list
+    Fonction qui analyse une ligne passée en paramètre.
+    --------------------------------------------------
+    laligne -> str
     return -> bool
     """
-    global ligne, current_variable, variables, commence, checksum, sauvegarde, fonctions, recordVariable, fichier
-    mots = laligne
-    #Si jamais une ligne commence par un #, c'est un commentaire.
-    if mots[0] == "debut":
-      commence = True
-      return
-    if mots[0].startswith("#"):
-          return
+    global fichier, ligne, current_variable, variables, commence, checksum, sauvegarde, fonctions, recordVariable, filepath
+    test = laligne.split(" ")
+    mots = []
+    for mot in test:
+      mots.append(mot)
     if commence:
-        #Si le programmeur souhaite afficher quelque chose...
         if mots[0] == "definir":
-            print(f"! Attention ! ligne {ligne}")
-            print("On ne peut pas définir de fonctions une fois le mot clé debut analysé")
+            print(f"Erreur ligne {ligne}")
+            print("Vous ne pouvez pas définir une fonction après l'emploi de debut.")
             return False
-        if mots[0] == 'afficher':
-          #On saute une ligne...
-          print()
-          for mot in mots[1:len(mots)]:
-            if not mot.startswith("#"):
-              print(mot+" ", end="")
-            else:
-              if mot[1:len(mots)] in variables:
-                print(str(variables[mot[1:len(mots)]])+" ", end="")
+        elif mots[0] == "afficher":
+            for mot in mots[1:len(mots)]:
+              if not mot.startswith("#"):
+                print(mot+" ", end="")
               else:
-                print(f"Erreur ligne {ligne}")
-                print("La variable n'existe pas.")
-                return False
-        elif mots[0] not in variables:
-          if mots[0] in fonctions:
-            debut = fonctions[mots[0]][0]
-            fin = fonctions[mots[0]][1]
-            print(fichier.readlines()[0])
-            for truc in fichier.readlines():
-                print(truc)
-                execute_ligne(truc)
-                print("Ca ff")
-            return
-          if mots[0][0].isdigit():
-            print(f"Erreur à la ligne {ligne}.")
-            print("Le nom d'une variable ne doit pas commencer par un chiffre.")
-            return False
-          if len(mots)>2:
-            variables[mots[0]] = None
-            current_variable = mots[0]
-            recordVariable = True
-            if mots[1] != "=":
-              print(f"Erreur ligne {ligne}.")
-              print("Attention ! Vous essayez de définir une variable sans utiliser le signe =")
-              return False
+                if mot[1:len(mots)] in variables:
+                  print(str(variables[mot[1:len(mots)]])+" ", end="")
+                else:
+                  print(f"Erreur ligne {ligne}")
+                  print("La variable n'existe pas.")
+                  return False
+            print()
+        elif len(mots) > 2 and mots[1] == "=":
+            if mots[2].isdigit():
+                variables[mots[0]] = int(mots[2])
+            elif mots[2].startswith("("):
+                variables[mots[0]] = analyse_expression(mots[2:len(mots)])
             else:
-                recordVariable = False
-                if mots[2][0].isdigit() and len(mots)== 3 and not "." in mots[2]:
-                    variables[current_variable] = int(mots[2])
-                elif mots[2][0].isdigit() and len(mots) == 3 and "." in mots[2]:
-                    variables[current_variable] = float(mots[2])
-                elif mots[2][0].isdigit() and len(mots)==5:
-                  if mots[4][0].isdigit():
-                    if mots[3] == "+":
-                      variables[current_variable] = float(mots[2]) + float(mots[4])
-                    elif mots[3] == "-":
-                      variables[current_variable] = float(mots[2]) - float(mots[4])
-                    elif mots[3] == "*":
-                      variables[current_variable] = float(mots[2]) * float(mots[4])
-                    elif mots[3] == "/":
-                      variables[current_variable] = float(mots[2]) / float(mots[4])
-                    else:
-                      print(f"Erreur ligne {ligne}")
-                      print("Opération mathématique non reconnue")
-                      return False
-                  else:
-                    print(f"Erreur ligne {ligne}")
-                    print("Pour faire une opération avec un nombre, il faut un deuxième nombre.")
-                    return False
-                elif mots[2].startswith('"') and mots[len(mots)-1].endswith('"'):
-                    tmp = mots[2:len(mots)]
-                    str_vide = ""
-                    for truc in tmp:
-                        if truc != tmp[len(tmp)-1]:
-                          str_vide = str_vide + truc + " "
-                        else:
-                          str_vide = str_vide + truc
-                    variables[current_variable] = str(str_vide[1:len(str_vide)-1])
-                elif mots[2].startswith('"') and not mots[len(mots)-1].endswith('"'):
-                    print(f"Erreur ligne {ligne}")
-                    print("Veuillez fermer votre chaîne de caractères par un guillemet.")
-                    return False
-                if mots[2] in variables:
-                    variables[current_variable] = variables[mots[2]]
-          else:
-              print()
-              print(f"Erreur ligne {ligne}")
-              print("Le premier mot n'est pas connu du langage.")
-              return False
-        else:
-          print(f"Erreur ligne {ligne}")
-          print("Une erreur de syntaxe s'est produite.")
-          return False
+                print(f"Erreur ligne {ligne}")
+                print("Erreur lors de l'affectation de la variable.")
+        elif mots[0] in fonctions:
+            file = open(filepath, "rt")
+            for loop in range(fonctions[mots[0]][0], fonctions[mots[0]][1]):
+                print(fichier.read().splitlines())
+                execute_ligne(fichier.read().splitlines()[loop])
     else:
-      if mots[0] == "definir" and mots[2]:
-          fonctions[mots[1]] = [ligne, None]
-          sauvegarde = mots[1]
-      elif mots[0] == "}":
-          fonctions[sauvegarde][1] = ligne
+        if mots[0] == "debut":
+            commence = True
+        elif mots[0] == "definir":
+          if len(mots) == 3 and mots[2]=="{":
+            fonctions[mots[1]] = [ligne, None]
+            checksum = checksum + 1
+            sauvegarde = mots[1]
+          else:
+            print(f"Erreur ligne {ligne}")
+            print("Définition de fonction incorrecte")
+            return False
+        elif mots[0] == "}":
+            checksum = checksum - 1
+            if checksum == 0 and sauvegarde:
+              fonctions[sauvegarde][1] = ligne
+              sauvegarde = None
+
+def analyse_expression(expression):
+    global ligne, current_variable, variables, commence, checksum, sauvegarde, fonctions, recordVariable
+    test = expression
+    if len(test) > 2:
+        if test[1] == "+":
+            return int(test[0][1:len(test[0])]) + int(test[2][0:len(test[2])-1])
+        elif test[1] == "-":
+            return int(test[0][1:len(test[0])]) - int(test[2][0:len(test[2])-1])
+        elif test[1] == "*":
+            return int(test[0][1:len(test[0])]) * int(test[2][0:len(test[2])-1])
+        elif test[1] == "/":
+            return int(test[0][1:len(test[0])]) / int(test[2][0:len(test[2])-1])
+        else:
+            print(f"Erreur ligne {ligne}")
+            print("Opération arithmétique non reconnue.")
+    else:
+        print(f"Erreur ligne {ligne}")
+        print("Expression incorrecte.")
 
 print("Les développeurs du langage Franco vous saluent !")
 #On vérifie si l'utilisateurs a passé un nom de fichier en paramètre
@@ -162,6 +116,9 @@ if len(sys.argv) > 1:
 else:
     filepath = input("Veuillez entrer un nom de fichier à lire : ")
 #On charge le fichier.
+if not filepath.endswith(".frl"):
+    print("Le fichier n'a pas une extension correcte.")
+    print("Essai d'ouverture...")
 try:
   fichier = open(filepath,'rt')
 except:
