b5ab618361dad0a23e5d0b413b6c12d0e21cf597
Geek du 25 <145868282+Geekdu25@users.noreply.github.com>
2024-02-16 16:06:32 +0100

    Update Franco-reader.py

--- src/Franco-reader.py
+++ src/Franco-reader.py
@@ -7,7 +7,7 @@ Ce programme python permet de lire les fichiers franco au format .frl
 #On importe la bibliothèque sys, qui nous permettera de charger un fichier passé en paramètre.
 import sys
 
-#On initialise quelques variables (numéro de ligne...)
+#On initialise quelques variables (numéro de ligne, la liste des variables, celle des fonctions...)
 ligne = 0
 current_variable = ""
 variables = {}
@@ -24,53 +24,78 @@ def read_frl(fichier):
   fichier -> fichier
   return -> bool
   """
+  #On rend certaines variables globales utilisables dans la fonction.
   global ligne, current_variable, variables, commence, checksum, sauvegarde, fonctions, recordVariable
+  #On décompose le fichier en plusieurs lignes (qu'on appelle truc).
   for truc in fichier.read().splitlines():
+    #On incrémente le numéro de ligne
     ligne = ligne + 1
+    #Et on exécute cette ligne via la fonction execute_ligne
+    #On stocke dans la variable result, un booléen. True si l'exécution de la ligne n'a pas déclenché d'erreur, False sinon.
     result = execute_ligne(truc)
+    #Et si jamais la fonction nous renvoie une erreur, on arrête le programme
     if result==False:
         return False
+  #Et si tout s'est bien passé on renvoie True    
   return True
 
 def execute_ligne(laligne):
     """
-    Fonction qui analyse une ligne passée en paramètre.
+    Fonction qui analyse une ligne passée en paramètre, et qui l'exécute.
     --------------------------------------------------
     laligne -> str
     return -> bool
     """
+    #On rend certaines variables globales utilisables dans la fonction.
     global fichier, ligne, current_variable, variables, commence, checksum, sauvegarde, fonctions, recordVariable, filepath
+    #On décompose la ligne en mots (qui sont séparés par des espaces).
     test = laligne.split(" ")
     mots = []
+    #Et on stocke tous ces mots dans une variable mots.
     for mot in test:
       mots.append(mot)
+    #Si l'utilisateur a déjà entré le mot clé debut...   
     if commence:
+        #Si le premier mot de la ligne est definir, on indique à l'utilisateur qu'il ne peut pas définir de fonctions aprés avoir écrit debut.
         if mots[0] == "definir":
             print(f"Erreur ligne {ligne}")
             print("Vous ne pouvez pas définir une fonction après l'emploi de debut.")
             return False
+        #Sinon, si le mot est afficher, on va afficher la phrase que l'utilisateur souhaite afficher.  
         elif mots[0] == "afficher":
+            #Pour chaque mot se trouvant après afficher...
             for mot in mots[1:len(mots)]:
+              #On n'a pas affaire à une variable, on va donc afficher le mot plus un espace.
               if not mot.startswith("#"):
                 print(mot+" ", end="")
+              #Sinon...  
               else:
+                #Si le mot se trouve dans le dictionnaire variables...
                 if mot[1:len(mot)] in variables:
+                  #On affiche le contenu de cette variable et un espace.
                   print(str(variables[mot[1:len(mot)]])+" ", end="")
                 else:
                   print(f"Erreur ligne {ligne}")
                   print("La variable n'existe pas.")
                   return False
+            #Et on fait un petit retour à la ligne.      
             print()
+        #Sinon, s'il y a plus de deux mots et qu'un espace se trouve en seconde position...  
         elif len(mots) > 2 and mots[1] == "=":
+            #Si le troisième mot est un nombre entier, on enregistre un nombre entier.
             if mots[2].isdigit() and not "." in mots[2]:
                 variables[mots[0]] = int(mots[2])
+            #Si il a une virgule on l'enregistre comme un flottant.  
             elif mots[2].isdigit() and "." in mots[2]:
                 variables[mots[0]] = float(mots[2])
+            #Si il commence par des guillemets...
+            #On enregistre cette variable comme une string...
             elif mots[2].startswith('"'):
                 str_vide = ""
                 for mot in mots[2:len(mots)]:
                     str_vide = str_vide + mot + " "
                 variables[mots[0]] = str_vide[1:len(str_vide)-2]
+            #Sinon, si le mot entré en troisième position est le mot entree  
             elif mots[2] == "entree":
                 str_vide = ""
                 for mot in mots[3:len(mots)]:
