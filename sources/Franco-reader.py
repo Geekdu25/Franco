164889a57923dd740cc12c2dd7f92a8f6a70d310
Geek du 25 <145868282+Geekdu25@users.noreply.github.com>
2024-03-20 11:01:28 +0100

    Update Franco-reader.py

--- sources/Franco-reader.py
+++ sources/Franco-reader.py
@@ -32,7 +32,6 @@ def read_frl(fichier):
   global ligne, current_variable, variables, commence, checksum, sauvegarde, fonctions, recordVariable, mode_si, current_checksum, numt
   #On décompose le fichier en plusieurs lignes (qu'on appelle truc).
   for truc in fichier.read().splitlines():
-    #On incrémente le numéro de ligne
     ligne = ligne + 1
     #Et on exécute cette ligne via la fonction execute_ligne
     #On stocke dans la variable result, un booléen. True si l'exécution de la ligne n'a pas déclenché d'erreur, False sinon.
@@ -70,7 +69,6 @@ def execute_ligne(laligne):
     laligne -> str
     return -> bool
     """
-    #On rend certaines variables globales utilisables dans la fonction.
     global fichier, ligne, current_variable, variables, commence, checksum, sauvegarde, fonctions, recordVariable, filepath, mode_si, current_checksum, numt
     #On décompose la ligne en mots (qui sont séparés par des espaces).
     test = laligne.split(" ")
@@ -79,10 +77,9 @@ def execute_ligne(laligne):
     for mot in test:
       if mot != "":
         mots.append(mot)
-    #Si l'utilisateur a déjà entré le mot clé debut...
     if len(mots) > 0:
         if commence:
-            #Si le premier mot de la ligne est definir, on indique à l'utilisateur qu'il ne peut pas définir de fonctions aprés avoir écrit debut.
+            #-------------------DEFINITION INTERDITE----------------------------
             if mots[0] == "definir":
                 print(f"Erreur ligne {ligne}")
                 print("Vous ne pouvez pas définir une fonction après l'emploi de debut.")
@@ -103,18 +100,13 @@ def execute_ligne(laligne):
               alors = analyse_expression(expression)
               if not alors:
                 mode_si = True
-            #Sinon, si le mot est afficher, on va afficher la phrase que l'utilisateur souhaite afficher.
+            #---------------------------AFFICHER------------------------------
             elif mots[0] == "afficher":
-                #Pour chaque mot se trouvant après afficher...
                 for mot in mots[1:len(mots)]:
-                  #On n'a pas affaire à une variable, on va donc afficher le mot plus un espace.
                   if not mot.startswith("#"):
                     print(mot+" ", end="")
-                  #Sinon...
                   else:
-                    #Si le mot se trouve dans le dictionnaire variables...
                     if mot[1:len(mot)] in variables:
-                      #On affiche le contenu de cette variable et un espace.
                       if type(variables[mot[1:len(mot)]]):
                         print(str(round(variables[mot[1:len(mot)]]))+" ", end="")
                       else:
@@ -123,17 +115,11 @@ def execute_ligne(laligne):
                       print(f"Erreur ligne {ligne}")
                       print("La variable n'existe pas.")
                       return False
-                #Et on fait un petit retour à la ligne.
                 print()
-            #Sinon, s'il y a plus de deux mots et qu'un singne égal se trouve en seconde position...
+            #----------------AFFECTATION DE VARIABLE--------------------
             elif len(mots) > 2 and mots[1] == "=":
-                #Si le troisième mot est un nombre entier, on enregistre un nombre entier.
-                if mots[2].isdigit() and not "." in mots[2]:
-                    variables[mots[0]] = int(mots[2])
-                #Si il a une virgule on l'enregistre comme un flottant.
-                elif mots[2].isdigit() and "." in mots[2]:
+                if mots[2].isdigit():
                     variables[mots[0]] = float(mots[2])
-                #Si on trouve un #, c'est une variable déjà enregistrée
                 elif mots[2].startswith("#"):
                   try:
                     variables[mots[0]] = variables[mots[2][1:len(mots[2])]]
@@ -141,40 +127,27 @@ def execute_ligne(laligne):
                     print(f"Erreur ligne {ligne}")
                     print(f"La variable {mots[2][1:len(mots[2])]}, n'existe pas.")
                     return False
-                #Si il commence par des guillemets...
-                #On enregistre cette variable comme une string...
                 elif mots[2] == "Vrai" or mots[2] == "Faux":
                   variables[mots[0]] = mots[2]
-                elif mots[2].startswith('"'):
-                    str_vide = ""
-                    for mot in mots[2:len(mots)]:
-                        str_vide = str_vide + mot + " "
-                    variables[mots[0]] = str_vide[1:len(str_vide)-2]
-                #Sinon, si le mot entré en troisième position est le mot entree
+                #--------------------INPUT----------------------------------
                 elif mots[2] == "entree":
-                    #On crée une chaîne vide (à laquelle oon rajoute des mots.) contenant la question
                     str_vide = ""
                     if len(mots) > 3:
                       for mot in mots[3:len(mots)]:
                           str_vide = str_vide + mot + " "
                     result = input(str_vide)
-                    #On convertit le résultat en nombre si c'est un nombre.
                     if result.isdigit():
                         result = float(result)
-                    #Et on enregistre cette valeur dans le dictionnaire.
                     variables[mots[0]] = result
-                #Sinon, si le mot commence par une parenthèse, on a affaire à une expression.
+                #-----------------------EXPRESSION-------------------------
                 elif mots[2].startswith("("):
-                    #Il faut donc l'analyser.
                     variables[mots[0]] = int(analyse_expression(mots[2:len(mots)]))
-                #Sinon, on retourne une erreur.
                 else:
                     print(f"Erreur ligne {ligne}")
                     print("Erreur lors de l'affectation de la variable.")
                     return False
-            #Sinon, si le mot est enregistré comme une fonction...
+            #--------------------EXECUTION DE FONCTION---------------------
             elif mots[0] in fonctions:
-                #On analyse chaque ligne du debut de la fonction jusqu'à sa fin.
                 file = open(filepath, "rt", encoding="UTF-8")
                 lignen = fonctions[mots[0]][0]
                 for truc in file.read().splitlines()[fonctions[mots[0]][0]:fonctions[mots[0]][1]]:
@@ -186,12 +159,12 @@ def execute_ligne(laligne):
               fonctions[f"tmp{numt}"] = [mots[1:len(mots)-1], ligne, None]
               numt = numt + 1
               checksum = checksum + 1
-        #Si le programme n'a pas encore commencé.
+        #Le programme n'a pas encore commencé
         else:
-            #On commence le programme avec le mot-clé debut
+            #------------------DEBUT-----------------
             if mots[0] == "debut":
                 commence = True
-            #Ou on définit une fonction avec le mot clé definir
+            #----------------DEFINITION DE FONCTION---------------------
             elif mots[0] == "definir":
               if len(mots) == 3 and mots[2]=="{":
                 fonctions[mots[1]] = [ligne, None]
@@ -201,7 +174,6 @@ def execute_ligne(laligne):
                 print(f"Erreur ligne {ligne}")
                 print("Définition de fonction incorrecte")
                 return False
-            #Ici, on termine l'enregistrement d'ne fonction car on a décovert une fin d'accolade
             elif mots[0] == "}":
                 checksum = checksum - 1
                 if checksum == 0 and sauvegarde:
